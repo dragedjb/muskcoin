5f929fa33f2b2c399a19d0c6b56b1d8ab856c0ce
dragedjb <jalenbrunson34@gmail.com>
2026-01-23 04:24:39 +0000

    finally

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,19 +1,13 @@
 const express = require('express');
-const router = express.Router()
-const cors = require('cors')
-const { test, registerUser, loginUser, getProfile, logoutUser } = require('../controllers/authController.js')
+const router = express.Router();
+// Remove the cors require and router.use(cors) block from here!
 
-//middleware
-router.use(
-    cors({
-        credentials: true,
-        origin: 'http://localhost:5173'
-    })
-)
-router.get('/', test)
-router.post('/register', registerUser)
-router.post('/login', loginUser)
-router.get('/profile', getProfile)
-router.post('/logout', logoutUser)
+const { test, registerUser, loginUser, getProfile, logoutUser } = require('../controllers/authController.js');
 
-module.exports = router
+router.get('/', test);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/profile', getProfile);
+router.post('/logout', logoutUser);
+
+module.exports = router;
